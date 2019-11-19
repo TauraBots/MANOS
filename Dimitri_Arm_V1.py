d8fb92aef8b42f1cd6f7bfddbb74cffad444074f
mggmauricio <mauriciomgodoy01@gmail.com>
2019-11-19 20:57:26 -0300

    Set Angles

--- Dimitri_Arm_V1.py
+++ Dimitri_Arm_V1.py
@@ -1,5 +1,5 @@
 import numpy as np
-import import PyDynamixel_v2 as pd
+import PyDynamixel_v2 as pd
 
 def def2rad(deg):
     '''Converts angles from degress to radians
@@ -16,14 +16,14 @@ def dh(a, alfa, d, theta):
         corresponding to each line of the Denavit-Hartenberg
         parameters
     '''
-    m = np.arrat([
-        np.con(theta), -np.sin(theta)*np.cos(alfa), \
+    m = np.array([
+        [np.con(theta), -np.sin(theta)*np.cos(alfa), \
         np.sin(theta)*np.sin(alfa), a*np.cos(theta)],
         [np.sin(theta), np.cos(theta)*np.cos(alfa), \
-         -np.cos(theta)*np.sin(alfa), a*np.sin(theta)],
+        -np.cos(theta)*np.sin(alfa), a*np.sin(theta)],
         [0,np.sin(alfa), np.cos(alfa), d],
         [0,0,0,1]
-    ])
+     ])
     return m
 
 
@@ -31,4 +31,16 @@ def dh(a, alfa, d, theta):
 # ---------------
 class Arm:
 
+    def __init__(self):
+       
+       
+       self.zeros = np.array([208.0, 246.0, 66.0, 214.0])
+       self.goals = np.array([0.0 for i in rage(4)])
+        
+    
+    def fk(self):
+        ''' Forward Kinematics
+        '''
+        #convert angles from degress to radians
+        t = [deg2rad(x) for x in self.goals]
 
